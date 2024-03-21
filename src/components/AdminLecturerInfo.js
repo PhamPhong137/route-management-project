72ac9c170a3e8d6b52c53b601d0bb17b4b17ae3c
PhamPhong137 <pp072003@gmail.com>
2024-03-21 13:57:37 +0700

    update Admin

--- src/components/AdminLecturerInfo.js
+++ src/components/AdminLecturerInfo.js
@@ -1,22 +1,40 @@
-import { Button, Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function AdminLecturerInfo() {
   const [users, setUser] = useState([]);
+  const [showDetail, setShowDetail] = useState(false);
+  const [detailUser, setDetailUser] = useState(null);
+
   useEffect(() => {
     fetchUsers();
   }, []);
+  const handleShowDetail = (user) => {
+    setDetailUser(user);
+    setShowDetail(true);
+  };
 
   const fetchUsers = async () => {
     const response = await axios.get("http://localhost:3000/users");
     setUser(response.data);
   };
+  const handleDelete = async (userId) => {
+    try {
+
+      await axios.delete(`http://localhost:3000/users/${userId}`);
+      fetchUsers();
+    } catch (error) {
+      console.error("Could not delete the user", error);
+    }
+  };
 
   return (
     <div>
       <h1 className="text-center mt-3">Lecturer Information</h1>
-      <Button variant="primary" className="mb-3">Create new lecturer</Button>
+      <Button variant="primary" className="mb-3">
+        Create new lecturer
+      </Button>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -41,13 +59,82 @@ function AdminLecturerInfo() {
               <td>{user.phone}</td>
               <td>{user.department}</td>
               <td>
-                <Button variant="primary" className="m-2">View detail</Button>
-                <Button variant="danger">Delete</Button>
+                <Button
+                  variant="primary"
+                  className="m-2"
+                  onClick={() => handleShowDetail(user)}
+                >
+                  View detail
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={() => handleDelete(user.id)} 
+                >
+                  Delete
+                </Button>
               </td>
             </tr>
           ))}
         </tbody>
       </Table>
+      <Modal show={showDetail} onHide={() => setShowDetail(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Lecturer Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {detailUser && (
+            <Table striped bordered hover>
+              <tbody>
+                <tr>
+                  <td>Email</td>
+                  <td>{detailUser.email}</td>
+                </tr>
+                <tr>
+                  <td>First Name</td>
+                  <td>{detailUser.firstName}</td>
+                </tr>
+                <tr>
+                  <td>Last Name</td>
+                  <td>{detailUser.lastName}</td>
+                </tr>
+                <tr>
+                  <td>Phone</td>
+                  <td>{detailUser.phone}</td>
+                </tr>
+                <tr>
+                  <td>Department</td>
+                  <td>{detailUser.department}</td>
+                </tr>
+                <tr>
+                  <td>Language Preference</td>
+                  <td>{detailUser.language_preference}</td>
+                </tr>
+                <tr>
+                  <td>Role</td>
+                  <td>{detailUser.role}</td>
+                </tr>
+                <tr>
+                  <td>Address</td>
+                  <td>{detailUser.address}</td>
+                </tr>
+                <tr>
+                  <td>Birthday</td>
+                  <td>{detailUser.birthday}</td>
+                </tr>
+                <tr>
+                  <td>Gender</td>
+                  <td>{detailUser.gender}</td>
+                </tr>
+              </tbody>
+            </Table>
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowDetail(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- src/components/AdminSlotBus.js
+++ src/components/AdminSlotBus.js
@@ -1,51 +1,212 @@
-import { Button, Table } from "react-bootstrap";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Button, Modal, Form, Table } from "react-bootstrap";
 
-function AdminSlotBus() {
+const AdminSlotBus = () => {
   const [slotBuses, setSlotBuses] = useState([]);
+  const [buses, setBus] = useState([]);
+  const [routes, setRoute] = useState([]);
+  const [slots, setSlot] = useState([]);
 
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [currentSlotBus, setCurrentSlotBus] = useState({
+    slot_id: "",
+    bus_id: "",
+    route_id: "",
+  });
+  const [isNewSlotBus, setIsNewSlotBus] = useState(true);
   useEffect(() => {
-    fetchSlotBuses();
+    fetchAllData();
   }, []);
 
-  const fetchSlotBuses = async () => {
-    const response = await axios.get("http://localhost:3000/slot_bus");
-    setSlotBuses(response.data);
-    console.log(response.data);
+  const fetchAllData = async () => {
+    try {
+      // Sử dụng Promise.all để gọi song song và tối ưu hiệu suất
+      const [slotBusesData, slotsData, busesData, routesData] =
+        await Promise.all([
+          axios.get("http://localhost:3000/slot_bus"),
+          axios.get("http://localhost:3000/slots"),
+          axios.get("http://localhost:3000/buses"),
+          axios.get("http://localhost:3000/routes"),
+        ]);
+
+      // Cập nhật state cho mỗi loại dữ liệu
+      setSlotBuses(slotBusesData.data);
+      setSlot(slotsData.data);
+      setBus(busesData.data);
+      setRoute(routesData.data);
+    } catch (error) {
+      console.error("Failed to fetch data:", error);
+    }
   };
+
+  const openModal = (
+    slotBus = { slot_id: "", bus_id: "", route_id: "" },
+    isNew = true
+  ) => {
+    setCurrentSlotBus(slotBus);
+    setIsNewSlotBus(isNew);
+    isNew ? setShowCreateModal(true) : setShowEditModal(true);
+  };
+
+  const closeModal = () => {
+    setShowEditModal(false);
+    setShowCreateModal(false);
+  };
+
+  const handleSave = async () => {
+    if (isNewSlotBus) {
+      await axios.post("http://localhost:3000/slot_bus", currentSlotBus);
+    } else {
+      await axios.put(
+        `http://localhost:3000/slot_bus/${currentSlotBus.id}`,
+        currentSlotBus
+      );
+    }
+    fetchAllData();
+    closeModal();
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`http://localhost:3000/slot_bus/${id}`);
+    fetchAllData();
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    const numberValue = Number(value);
+    setCurrentSlotBus((prevState) => ({ ...prevState, [name]: numberValue }));
+  };
+
   return (
     <>
-      <h1 className="text-center mt-3">Slot Bus Information</h1>
-      <Button variant="primary" className="mb-3">
-        Create new slot bus
+      <h1 className="text-center mt-3">Slot_Bus Information</h1>
+      <Button variant="primary" onClick={() => openModal(undefined, true)}>
+        Create Slot Bus
       </Button>
-      <Table striped bordered hover>
+      <Table striped bordered hover className="mt-3">
         <thead>
           <tr>
-            <th>Id</th>
-            <th>Slot_Id</th>
-            <th>Bus_Id</th>
-            <th>RouteId</th>
+            <th>ID</th>
+            <th>Slot ID</th>
+            <th>Bus ID</th>
+            <th>Route ID</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {slotBuses.map((slotBus) => (
-            <tr key={slotBus.id}>
-              <td>{slotBus.id}</td>
-              <td>{slotBus.slot_id}</td>
-              <td>{slotBus.bus_id}</td>
-              <td>{slotBus.route_id}</td>
-              <td>
-                <Button variant="primary" className="m-2">Edit</Button>
-                <Button variant="danger">Delete</Button>
-              </td>
-            </tr>
-          ))}
+          {slotBuses.map((slotBus) => {
+            // Tìm thông tin chi tiết về slot, bus và route dựa trên ID
+            const slot = slots.find((s) => s.id === slotBus.slot_id);
+            const bus = buses.find((b) => b.id === slotBus.bus_id);
+            const route = routes.find((r) => r.id === slotBus.route_id);
+
+            return (
+              <tr key={slotBus.id}>
+                <td>{slotBus.id}</td>
+                <td>
+                  {slot
+                    ? `${slot.week_day} - ${slot.period} | Time: ${slot.departure_time} -> ${slot.arrival_time}`
+                    : "Not Found"}
+                </td>
+                <td>
+                  {bus
+                    ? `${bus.license_plate} - Capacity: ${bus.capacity}`
+                    : "Not Found"}
+                </td>
+                <td>
+                  {route
+                    ? `${route.route_name} | ${route.start_location} -> ${route.end_location}`
+                    : "Not Found"}
+                </td>
+                <td>
+                  <Button
+                    variant="secondary"
+                    onClick={() => openModal(slotBus, false)}
+                  >
+                    Edit
+                  </Button>{" "}
+                  <Button
+                    variant="danger"
+                    onClick={() => handleDelete(slotBus.id)}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
+
+      <Modal show={showEditModal || showCreateModal} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {isNewSlotBus ? "Create Slot Bus" : "Edit Slot Bus"}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="slotId">
+              <Form.Label>Slot</Form.Label>
+              <Form.Control
+                as="select"
+                name="slot_id"
+                value={currentSlotBus.slot_id}
+                onChange={handleChange}
+              >
+                {slots.map((slot) => (
+                  <option key={slot.id} value={slot.id}>
+                    {`${slot.week_day} - ${slot.period} | Time: ${slot.departure_time} -> ${slot.arrival_time}`}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="busId">
+              <Form.Label>Bus</Form.Label>
+              <Form.Control
+                as="select"
+                name="bus_id"
+                value={currentSlotBus.bus_id}
+                onChange={handleChange}
+              >
+                {buses.map((bus) => (
+                  <option key={bus.id} value={bus.id}>
+                    {`${bus.license_plate} - Capacity: ${bus.capacity}`}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="routeId">
+              <Form.Label>Route</Form.Label>
+              <Form.Control
+                as="select"
+                name="route_id"
+                value={currentSlotBus.route_id}
+                onChange={handleChange}
+              >
+                {routes.map((route) => (
+                  <option key={route.id} value={route.id}>
+                    {`${route.route_name} | ${route.start_location} -> ${route.end_location}`}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
-}
+};
 
 export default AdminSlotBus;
